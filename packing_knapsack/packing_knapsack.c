836208eb65da36cdaa691079e543ebccdfc23a66
Taeung Song <treeze.taeung@gmail.com>
2016-03-25 14:59:24 +0900

    packing knapsack: Implement code to solve this question
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- packing_knapsack/packing_knapsack.c
+++ packing_knapsack/packing_knapsack.c
@@ -12,33 +12,61 @@
 #define MAX_INPUT 16
 
 struct knapsack {
-	unsigned int limited_wgt;
-	unsigned int max_price;
-};
+	unsigned int maxprice;
+} *knapsack_list;
 
 struct jewelry {
 	unsigned int wgt;
 	unsigned int price;
 };
 
-void pack_knapsack(struct knapsack *knapsack_list,
-		      struct jewelry **usable_jewels)
+unsigned int limited_wgt;
+
+unsigned int get_cond_maxprice(int wgt, struct jewelry *jewelry)
+{
+	/* Get maximum price based on a specific weight
+	 * following a specific jewelry.
+	 */
+	int i;
+	int rest_wgt = wgt - jewelry->wgt;
+	int price = jewelry->price + knapsack_list[rest_wgt].maxprice;
+
+	return knapsack_list[wgt].maxprice < price ?
+		price : knapsack_list[wgt].maxprice;
+}
+
+void pack_knapsack(struct jewelry *jewelry)
 {
 	/* Case by case pack knapsack following maximum
 	 * price per limited weight.
 	 */
+	int wgt;
+
+	if (limited_wgt < jewelry->wgt)
+		return;
+
+	for (wgt = 0; wgt <= limited_wgt; wgt++) {
+		if (jewelry->wgt <= wgt) {
+			unsigned int maxprice = get_cond_maxprice(wgt, jewelry);
+
+			if (knapsack_list[wgt].maxprice < maxprice)
+				knapsack_list[wgt].maxprice = maxprice;
+		}
+	}
 }
 
-int get_maxprice(struct knapsack *knapsack_list, struct jewelry *jewelry)
+bool get_values_from(char *input, unsigned int *val1, unsigned int *val2)
 {
-	/* Get maximum price based on limited weight */
-}
+	char *arg;
+	char *ptr = strdup(input);
 
-bool get_values_from(char *input, int *val1, int *val2)
-{
-	char *ptr = input;
+	if (!ptr) {
+		printf("%s: strdup failed\n", __func__);
+		exit(1);
+	}
 
-	*val1 = atoi(strsep(&ptr, " "));
+	arg = strsep(&ptr, " ");
+	*val1 = atoi(arg);
 	if (ptr == NULL) {
 		printf("Error: Need a whitespace\n");
 		return false;
@@ -46,28 +74,22 @@ bool get_values_from(char *input, int *val1, int *val2)
 
 	*val2 = atoi(ptr);
 
-	if (*val1 == 0 || *val2 == 0) {
-		printf("Error: Input have to contain only numbers\n");
-		return false;
-	}
-
 	return true;
 }
 
 int main(int argc, const char **argv)
 {
-	char input[MAX_INPUT];
-	char *ptr = input;
-	int nr_jewels, limited_wgt;
+	int i;
 	struct jewelry *jewels;
-	struct knapsack *knapsack_list;
+	char input[MAX_INPUT];
+	unsigned int nr_jewels;
 
 	fgets(input, sizeof(input), stdin);
 	if (get_values_from(input, &nr_jewels, &limited_wgt) == false)
 		return -1;
 
 	jewels = malloc(sizeof(struct jewelry) * nr_jewels);
-	for (int i = 0; i < nr_jewels; i++) {
+	for (i = 0; i < nr_jewels; i++) {
 		bool ret;
 
 		fgets(input, sizeof(input), stdin);
@@ -77,19 +99,16 @@ int main(int argc, const char **argv)
 			return -1;
 	}
 
-	knapsack_list = malloc(sizeof(struct knapsack) * limited_wgt);
+	/* from 0 to last limited weight */
+	knapsack_list = malloc(sizeof(struct knapsack) * (limited_wgt + 1));
 
-	for (int i = 0; i < nr_jewels; i++) {
-		struct jewelry **usable_jewels = malloc(sizeof(struct jewelry *)
-							* (i + 1));
+	for (i = 0; i <= limited_wgt; i++)
+		knapsack_list[i].maxprice = 0;
 
-		for (int j = 0; j <= i; j++)
-			usable_jewels[i] = &jewels[j];
-		pack_knapsack(knapsack_list, usable_jewels);
-		free(usable_jewels);
-	}
+	for (i = 0; i < nr_jewels; i++)
+		pack_knapsack(&jewels[i]);
 
-	printf("%d\n", knapsack_list[limited_wgt-1].max_price);
+	printf("%d\n", knapsack_list[limited_wgt].maxprice);
 	free(jewels);
 	free(knapsack_list);
 	return 0;
