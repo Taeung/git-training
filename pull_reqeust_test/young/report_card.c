e65d5b571c31986a4240c0cb515fd1bd3c451085
tao0001 <tao0001@nate.com>
2018-06-23 17:01:57 +0900

    test pull request
    Signed-off-by: tao0001 <tao0001@nate.com>

---
+++ pull_reqeust_test/young/report_card.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#to make diff for version control test
+## to make difference of signature 
+#
+#try again
+#
+#try 3rd
+#
+int main()
+{
+	int kor, eng, math;
+	int sum = 0;
+
+	math = 80;
+	eng = 100;
+	kor = 90;
+	sum = 80 + 100 + 90;
+
+	printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
+	printf("Sum : %d\n", sum);
+	printf("Mean : %d\n", sum/3);
+	return 0;
+}
