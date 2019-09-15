caea6f1bfe13eff49ca1e3b141722cd882b2c0c2
Sehee Lee <1234asdfgh5678@gmail.com>
2019-09-15 21:38:42 +0900

    report card: Show the sum of each grade
    Signed-off-by: Sehee Lee <1234asdfgh5678@gmail.com>

--- report_card.c
+++ report_card.c
@@ -3,15 +3,18 @@
 int main()
 {
 	int kor, eng, math;
+	int sum = 0;
 
 	math = 80;
 	eng = 100;
 	kor = 90;
+	sum = 80 + 100 + 90;
 
 	printf("This program print report card.\n");
 
 	printf("Korean : %d\n", kor);
 	printf("English : %d\n", eng);
 	printf("Math : %d\n", math);
+	printf("Sum : %d\n", sum);
 	return 0;
 }
