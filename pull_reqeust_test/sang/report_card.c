a32d0828c27a48cb7626f2c44b3759c336f77e61
Noh SangHyeon <shtkdgus9080@gmail.com>
2019-09-16 20:38:17 +0900

    test pull request
    Signed-off-by: Noh SangHyeon <shtkdgus9080@gmail.com>

--- pull_reqeust_test/sang/report_card.c
+++ pull_reqeust_test/sang/report_card.c
@@ -2,20 +2,5 @@
 
 int main()
 {
-	int kor, eng, math;
-	int sum = 0;
-
-	math = 80;
-	eng = 100;
-	kor = 90;
-	sum = 80 + 100 + 90;
-
-	printf("This program print report card.\n");
-
-	printf("Korean : %d\n", kor);
-	printf("English : %d\n", eng);
-	printf("Math : %d\n", math);
-	printf("Sum : %d\n", sum);
-	printf("Mean : %d\n", sum/3);
 	return 0;
 }
