44613bc976e4ca6b13481db312474e23d1f5ed03
nowwater <6360879@naver.com>
2020-02-10 10:18:43 +0900

    report card: Print grades of each subject

--- report_card.c
+++ report_card.c
@@ -2,6 +2,16 @@
 
 int main()
 {
+	int kor, eng, math;
+
+	math = 80;
+	eng = 100;
+	kor = 90;
+
 	printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
 	return 0;
 }
